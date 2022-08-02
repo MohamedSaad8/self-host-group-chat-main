a779bb1ab2e5a068c08742f30034e78f3e002cda
mohamedsaad8 <mohamedsaad.cis@gmail.com>
2022-08-02 16:26:30 +0200

    first commit

---
+++ lib/features/domain/entities/user_entity.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+
+  final String name;
+  final String email;
+  final String phoneNumber;
+  final bool isOnline;
+  final String uid;
+  final String status;
+  final String profileUrl;
+  final String password;
+  final String dob;
+  final String gender;
+
+  UserEntity({
+    this.name="",
+    this.email="",
+    this.phoneNumber="",
+    this.isOnline = false,
+    this.uid="",
+    this.status = "Hello there i'm using this app",
+    this.profileUrl="",
+    this.password="",
+    this.dob="",
+    this.gender="",
+  });
+
+  @override
+  List<Object> get props => [
+    name,
+    email,
+    phoneNumber,
+    isOnline,
+    uid,
+    status,
+    profileUrl,
+    password,
+    dob,
+    gender,
+  ];
+}
